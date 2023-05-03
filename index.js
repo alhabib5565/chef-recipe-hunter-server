2fe97c3c527c5fd3376c98c8b9f8a5d10ee44fbd
al-habib <alhabib5565@gmail.com>
2023-05-03 22:48:38 +0600

    data added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const data = require('./data/data.json')
 
 app.get('/', (req, res) => {
   res.send('server running for assignment 10')
 })
 app.get('/data', (req, res) => {
-  res.send('server running for assignment 10........................')
+  res.send(data)
 })
 
 app.listen(port, () => {
