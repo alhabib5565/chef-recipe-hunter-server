0b63c9e3d43a94159436e7e33b9741b72d523396
al-habib <alhabib5565@gmail.com>
2023-05-03 23:18:42 +0600

    find a  chef info

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const data = require('./data/data.json')
 app.get('/', (req, res) => {
   res.send('server running for assignment 10')
 })
-app.get('/data', (req, res) => {
+
+app.get('/alldata', (req, res) => {
   res.send(data)
+})
+app.get('/alldata/:id', (req, res) => {
+  const id = req.params.id
+  res.send(data.find(data => data.chef_id == id))
+  console.log(id)
 })
 
 app.listen(port, () => {
